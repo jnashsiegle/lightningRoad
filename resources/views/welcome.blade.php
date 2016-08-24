727e34658e3d192e9fa89add2be41ce2bb39d9bc
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2016-08-24 17:14:03 -0500

    fix google analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,16 @@
     </head>
     <body>
     <!--google analytics tracking  - MOVE TO MASTER BLADE TEMPLATE when developed -->
-    <?php include_once("analyticstracking.php") ?> 
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83128316-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
         <div class="container">
             <div class="content">
                 <div class="title">Lightning Road</div>
