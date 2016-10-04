ea719992c4cd0ff19abd3f74dbaab439c83cdae6
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-04 00:50:28 +0000

    change to register back

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
 						</div><!--end of row fluid-->
 						<div class = "row-fluid">
 						<div class = "form-group col-md-12 column">							
-								<a href="{{ URL::previous() }}" class="btn btn-primary btn-close btn-responsive">Cancel</a>
+								<a href="{{ URL('users') }}" class="btn btn-primary btn-close btn-responsive">Cancel</a>
 								<button type ="submit" class ="btn btn-default btn-send btn-responsive pull-right"> Submit </button> 
 						</div> <!--end of form group-->
 						</div><!--end of row fluid-->
