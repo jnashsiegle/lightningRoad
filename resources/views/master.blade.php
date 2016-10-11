fa687c1689efc00c405aeeeb81c1c0869407bb37
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-11 19:14:36 +0000

    optimized bootstrap.js

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -58,7 +58,7 @@
     <!-- jQuery -->
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
      <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script src = "https://cdn.jsdelivr.net/bootstrap/3.3.6/js/bootstrap.js"></script>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js" integrity="sha256-IxYUmOOk74FUrcx5FEMOHVmTJDb7ZAwnC/ivo/OQGxg=" crossorigin="anonymous"></script>
     <!--Font Awesome CDN-->
     {{-- <script async src="https://use.fontawesome.com/fe25733dc5.js"></script> --}}
