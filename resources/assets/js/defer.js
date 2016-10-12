1ecf8243de75588cf2ff47581fd581fbd55f00ed
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-12 23:21:02 +0000

    optimizing

--- resources/assets/js/defer.js
+++ resources/assets/js/defer.js
@@ -1,14 +1,21 @@
 /*google analytics tracking  */
    
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ /* (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-83128316-1', 'auto');
   ga('require', 'linkid');
-  ga('send', 'pageview');
+  ga('send', 'pageview');*/
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','http://www.lightningroadfp.tech/ga/analytics.js','ga');ga('create', 'UA-83128316-1', 'auto');
+ga('send', 'pageview');
+
+/* End Google Analytics */
   //hide and show text field and character count
 // dependant on what is selected in the subject.
 
