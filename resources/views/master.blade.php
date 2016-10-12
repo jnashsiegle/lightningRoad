b02efd4d8b8686ea53228bc19d68653fa36ee2d9
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-12 23:46:07 +0000

    optimizing

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,10 +16,6 @@
         commission, hays county detention center, texas mentoring, faith-based mentoring, motorcycle outreach, christian bikers, biker evangelism">
         <meta name = "author" content = "Jana Nash-Siegle">
         <link rel="canonical" href="http://lightningroadfp.tech/">
-        <?php
-       //cache control
-          header("Cache-Control: public, max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
-        ?>
         <link rel="shortcut icon" href="../images/logos/alpha_icon.ico" type="image/png">
         <link rel="icon" href="../images/logos/alpha_icon.ico" type="image/png">
         
