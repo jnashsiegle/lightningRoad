4f20bd9a75295cff84a0101fae2c8e8a429bf220
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-13 00:14:04 +0000

    optimizing

--- resources/assets/js/defer.js
+++ resources/assets/js/defer.js
@@ -12,13 +12,12 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','http://www.lightningroadfp.tech/ga/analytics.js','ga');ga('create', 'UA-83128316-1', 'auto');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-83128316-1', 'auto');
 ga('send', 'pageview');
 
 /* End Google Analytics */
   //hide and show text field and character count
 // dependant on what is selected in the subject.
-
 
 $(document).ready(function(){
 
@@ -57,4 +56,5 @@ else{
         $('#charCount').text(char + ' characters left');
     }
   });
+
 }); //end of first document.
