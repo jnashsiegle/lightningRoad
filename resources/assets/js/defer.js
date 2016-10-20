ebd0e8e722dd79daaa90137b7f4ee7c34f2be08f
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-20 05:53:25 +0000

    analytics fix

--- resources/assets/js/defer.js
+++ resources/assets/js/defer.js
@@ -1,78 +1,48 @@
-/*google analytics tracking  */
-   
- /* (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-83128316-1', 'auto');
-  ga('require', 'linkid');
-  ga('send', 'pageview');*/
-
-/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-83128316-1', 'auto');
-ga('send', 'pageview');*/
-
-/* End Google Analytics */
-
-  
-
 
   //hide and show text field and character count
 // dependant on what is selected in the subject.
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-//Subject Other Hide & Show
+    //Subject Other Hide & Show
 
-$('#subject').change(function(){
+    $('#subject').change(function() {
 
-var val =$("#subject :selected").val();
+        var val = $("#subject :selected").val();
 
-if(val === 'Other'){
-      $('#customOther').show() && 
-      $('#count-subject').show() &&
-      $('#charCount').show();
-}
-else{
-     $('#customOther').hide() && 
-      $('#count-subject').hide() &&
-      $('#charCount').hide();
+        if (val === 'Other') {
+            $('#customOther').show() &&
+                $('#count-subject').show() &&
+                $('#charCount').show();
+        } else {
+            $('#customOther').hide() &&
+                $('#count-subject').hide() &&
+                $('#charCount').hide();
 
-		}
-	})
+        }
+    })
 
-//google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    // Fade in & out of messages
 
-  ga('create', 'UA-83128316-1', 'auto');
-  ga('send', 'pageview');
-// Fade in & out of messages
+    $(".alert").fadeIn().delay(5000).fadeOut();
 
-	$(".alert").fadeIn().delay(5000).fadeOut();
-	 
-	//Count characters in optional subject field
-  
-  $('#customOther').keyup(function() {
-    var max = 50;
-    var len = $(this).val().length;
-    if (len >= max) {
-        $('#charCount').text(' you have reached the limit');
-    } else {
-        var char = max - len;
-        $('#charCount').text(char + ' characters left');
-    }
-  });
+    //Count characters in optional subject field
+
+    $('#customOther').keyup(function() {
+        var max = 50;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#charCount').text(' you have reached the limit');
+        } else {
+            var char = max - len;
+            $('#charCount').text(char + ' characters left');
+        }
+    });
 
 
- $('#userfile').on('change',function(){
-       // output raw value of file input
-       $('#userfilename').html($(this).val()); 
+    $('#userfile').on('change', function() {
+        // output raw value of file input
+        $('#userfilename').html($(this).val());
 
         // or, manipulate it further with regex etc.
         var userfilename = $(this).val();
