3c88ef52c13bd7e3cd2e9c5f20955fd66eef48a3
jnashsiegle <jsiegle@jlsdesk.com>
2016-10-20 02:37:44 +0000

    minified all.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,9 +30,10 @@ var elixir = require('laravel-elixir');
     'public/js/all.min.js');
 });*/
 
+
 /*UNCSS GULP and Sourcemap and Minify*/
 
-var gulp = require('gulp');
+/*var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
@@ -62,7 +63,7 @@ gulp.task('default', function () {
         .pipe(nano())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/css/app.min.css'));
-});
+});*/
 
 /*UNCSS GULP and Sourcemap and Minify for BACKEND*/
 
