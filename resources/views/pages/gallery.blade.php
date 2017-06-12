c1e92fc48219082ff56221d6a7d79e64b7a68ce8
jnashsiegle <jsiegle@fullsail.edu>
2017-06-12 17:27:48 +0000

    add images

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -95,9 +95,9 @@
             <a href = "../images/outreach/WitnessteamatHCJC_DayofChampionsevent.jpg" data-lightbox="outreach" data-title = "Day of Champions - Hays County Juvenile Center"></a>
             <a href = "../images/outreach/WorkingCopsforKidsoutreach.jpg" data-lightbox="outreach" data-title = "Cops for Kids Outreach"></a>
             <a href = "../images/fullsize/DayofChampions_HCJC.jpg" data-lightbox="outreach" data-title = "Day of Champions - Hays County Juvenile Center"></a> 
-            <a href = "../images/fullsize/HaysCo_Juvenile_Bible_delivery.jpg" data-lightbox="roadtrip" data-title = "Delivering Bibles at the Hays County Juvenile Center"></a>
-            <a href = "../images/fullsize/JohnnySpinks_Jesse_HCJC_PINKGLOVES.jpg" data-lightbox="roadtrip" data-title = "Johnny Spinks"></a>
-            <a href = "../images/fullsize/JohnnySpinks_Jesse_HCJC.jpg" data-lightbox="roadtrip" data-title = "Johnny Spinks"></a>
+            <a href = "../images/fullsize/HaysCo_Juvenile_Bible_delivery.jpg" data-lightbox="outreach" data-title = "Delivering Bibles at the Hays County Juvenile Center"></a>
+            <a href = "../images/fullsize/JohnnySpinks_Jesse_HCJC_PINKGLOVES.jpg" data-lightbox="outreach" data-title = "Johnny Spinks"></a>
+            <a href = "../images/fullsize/JohnnySpinks_Jesse_HCJC.jpg" data-lightbox="outreach" data-title = "Johnny Spinks"></a>
         </div>
 
         <div class = "col-md-4">
